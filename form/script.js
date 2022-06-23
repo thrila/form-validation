e4332f83b0501d3209626fb68eb852718bdf358c
Ifeanyichukwu Ndubuizu Michael <79278137+thrila@users.noreply.github.com>
2022-06-23 22:25:39 +0100

    Add files via upload

---
+++ form/script.js
@@ -0,0 +1,17 @@
+const uname = document.getElementById('uname')
+const password = document.getElementById('password')
+const error = document.getElementById('error')
+const form = document.getElementById('form')
+
+
+button.addEventListener('click' , valid );
+
+function valid (){
+if (uname.value == '' || password.value == '')[
+ error.innerHTML = " make sure you fill all fields"
+// alert('" make sure you fill all fields"')
+]
+else {
+
+}
+}
